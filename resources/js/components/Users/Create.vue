5b43a1a2b5dbd0dcc6cf1f2122799fc0722c0112
Sagit Gutierrez <jozaguts@gmail.com>
2021-07-14 14:41:52 -0500

    Create user page added

---
+++ resources/js/components/Users/Create.vue
@@ -0,0 +1,15 @@
+<template>
+<h1>test</h1>
+</template>
+
+<script>
+import UserForm from './Form'
+export default {
+    name: "Create",
+    components: {UserForm},
+}
+</script>
+
+<style scoped>
+
+</style>
