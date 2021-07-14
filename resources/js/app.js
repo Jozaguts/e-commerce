4474ee5fe83d5c86d4a2ef3d07ee1c6c0bead383
Sagit Gutierrez <jozaguts@gmail.com>
2021-07-14 14:46:08 -0500

    axios interceptor configured

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,17 @@
 require('./bootstrap');
-
 import Vue from 'vue'
+import Axios from 'axios'
 
+Vue.prototype.$http = Axios;
+const token = JSON.parse( localStorage.getItem('vuex'))
+if (token?.auth?.token) {
+    Vue.prototype.$http.defaults.headers.common['Authorization'] = 'Bearer '+token?.auth?.token
+}
 import vuetify from './plugins/vuetify'
 import router from './router'
 import App from "./App.vue";
 import store from './store'
-// creating a vue instance
+
 const app = new Vue({
     el: '#app',
     vuetify,
