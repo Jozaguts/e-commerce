a1d686fd87701115e01dfa21b5013f2eb37a169a
Sagit Gutierrez <jozaguts@gmail.com>
2021-07-14 14:40:20 -0500

    user delete-btn component added

---
+++ resources/js/components/Users/DeleteBtn.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-btn
+        @click="destroy(id)">
+        <v-icon color="red" v-text="icon">
+
+        </v-icon>
+    </v-btn>
+</template>
+
+<script>
+export default {
+    name: "DeleteBtn",
+    props:{
+        id:{
+            required:true,
+            type:Number,
+        },
+        icon:{
+            required:false,
+            type:String,
+            default: 'mdi-trash-can'
+        }
+    },
+    data() {
+        return {
+            isLoading:false
+        }
+    },
+    methods:{
+        destroy(id) {
+            this.isLoading = true
+            this.$store.dispatch('users/delete', id)
+                .then(res =>{
+                    if( res.success ){
+                        this.isLoading = false
+                    }
+                })
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
